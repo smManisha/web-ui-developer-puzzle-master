cd9863f318179fcb36b89627de958cdfd18a92de
smManisha <134503978+smManisha@users.noreply.github.com>
2023-05-26 15:29:31 +0530

    Implemented Undo add and remove from reading list functionality

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
-
+import { getReadingList, removeFromReadingList, addToReadingList } from '@tmo/books/data-access';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Book } from '@tmo/shared/models';
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
@@ -10,9 +11,19 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private _snackBar: MatSnackBar,
+    ) {}
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+    let snackBarRef = this._snackBar.open(`${item.title} is removed from Reading List`, 'Undo', {
+      duration: 3000
+    });
+    let book:Book = item;
+    snackBarRef.onAction().subscribe(() => {
+      this.store.dispatch(addToReadingList({ book }));
+   });
   }
 }
